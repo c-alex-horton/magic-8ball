479c8fdef228a0084e27cce01b305ea17c315779
Caleb Horton <c.alex.horton@gmail.com>
2021-08-13 16:09:08 -0500

    finshed script with base functionality and 8 responses. tweaked HTML

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 function returnAnswer() {
     function getAnswer() {
-        let num = Math.floor(Math.random() * 7);
+        let num = Math.floor(Math.random() * 8);
 
-        
+        switch (num) {
+            case 0:
+                return "That is a resounding NO.";
+                break;
+            case 1:
+                return "Improbible, but not impossible!";
+                break;
+            case 2:
+                return "I highly doubt it...";
+                break;
+                
+            case 3:
+                return "Oh, absolutley!";
+                break;
+            case 4:
+                return "Yes, but not in the way you think.";
+                break;
+            case 5:
+                return "No, but not in the way you think.";
+                break;
+            case 6:
+                return "HA! No way!";
+                break;
+            case 7:
+                return "I dunno, probably.";
+                break;
+            default:
+                return "Hmmm something went amiss. Try again."
+        }
+
+
     }
 
     document.getElementById("answer").innerHTML = getAnswer();
 }
+
+document.getElementById("ask").onclick = function(){returnAnswer()};
